cd39aff97ca31adf4e8a214a9f492758357e5916
Дмитрий Тяпин <tyapin2002@mail.ru>
2022-04-11 20:24:50 +0400

    Переписал задачу "Векторы", изменив характеристические векторы из string на vector<bool>

--- 2nd Semestr/Source.cpp
+++ 2nd Semestr/Source.cpp
@@ -1,57 +1,265 @@
 #include <iostream>
+#include <string>
 #include <vector>
+#include <Windows.h>
 
 using std::cout;
 using std::cin;
 using std::endl;
+using std::string;
 using std::vector;
 
+//////////////////////////////////////////////////////////////////////
+
+// Печать элементов строки через пробел
 template <class t>
-t max_elem_pos(vector<t> a);
+void print(t);
+// Печать элементов строки в виде множества
 template <class t>
-t min_elem_pos(vector<t> a);
+void printBraces(t);
+
+
+// Сортировка по возрастанию всех элементов строки
+string sort(string);
+// Удаление повторяющихся элементов строки
+string clean(string);
+
+
+// Вариант 17; Bin - для работы с характеристическими векторами
+string arrayR(string, string, string);
+vector<bool> arrayRBin(vector<bool>, vector<bool>, vector<bool>);
+// A / B
+string razn(string, string);
+vector<bool> raznBin(vector<bool>, vector<bool>);
+// A или B
+string AorB(string, string);
+vector<bool> AorBBin(vector<bool>, vector<bool>);
+// Создание характеристического вектора
+vector<bool> makeVector(string, string);
+
+//////////////////////////////////////////////////////////////////////
+
+class Alphabet
+{
+private:
+	string str;	// Изначальная строка
+	string a;	// Строка-алфавит
+public:
+	Alphabet();
+
+	// Вводим строку
+	void set() { cin >> str; }
+	// Возвращаем строку-алфавит
+	string get() { return a; }
+};
+
+//////////////////////////////////////////////////////////////////////
 
 int main()
 {
-	vector<int> arr;
-	int max_pos, min_pos, n;
-	cin >> n;
-	arr.resize(n);
-	for (int i = 0; i < n; ++i) cin >> arr[i];
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
 
-	max_pos = max_elem_pos(arr);
-	min_pos = min_elem_pos(arr);
-	if (max_pos + min_pos <= arr.size()) cout << arr[max_pos + min_pos - 1];
-	else cout << "Vne massiva";
+	Alphabet A;
+	Alphabet B;
+	Alphabet C;
 
+	string u = AorB(A.get(), B.get());
+	u = AorB(u, C.get());
+	vector<bool> av = makeVector(u, A.get());
+	vector<bool> bv = makeVector(u, B.get());
+	vector<bool> cv = makeVector(u, C.get());
+	vector<bool> rv = arrayRBin(av, bv, cv);
+	print(u);
+	print(av);
+	print(bv);
+	print(cv);
+	print(rv);
+
+	system("pause");
 	return 0;
 }
 
+//////////////////////////////////////////////////////////////////////
+
 template <class t>
-t max_elem_pos(vector<t> a)
+void print(t str)
 {
-	int pos = 0, max = -1;
-	for (int i = 0; i < a.size(); ++i)
+	for (int i = 0; i < str.size(); ++i)
 	{
-		if (a[i] > max)
-		{
-			max = a[i];
-			pos = i;
-		}
+		if (i < str.size() - 1) cout << str[i] << ' ';
+		else cout << str[i];
 	}
-	return ++pos;
+	cout << endl;
 }
 template <class t>
-t min_elem_pos(vector<t> a)
+void printBraces(t str)
 {
-	int pos = 0, min = 1000;
-	for (int i = 0; i < a.size(); ++i)
+	cout << "{";
+	for (int i = 0; i < str.size(); ++i)
 	{
-		if (a[i] < min)
+		if (i < str.size() - 1) cout << str[i] << ", ";
+		else cout << str[i] << "}" << endl;
+	}
+}
+
+string razn(string a, string b)
+{
+	string r;
+	int i = 0, j = 0;
+	while (j < b.length())
+	{
+		if (a[i] == b[j])
 		{
-			min = a[i];
-			pos = i;
+			++i;
+			++j;
+		}
+		else if (a[i] < b[j])
+		{
+			r.push_back(a[i]);
+			++i;
+		}
+		else if (a[i] > b[j])
+		{
+			++j;
 		}
 	}
-	return ++pos;
+	return r;
+}
+string AorB(string a, string b)
+{
+	string r;
+	int i = 0, j = 0;
+	while (i < a.length() && j < b.length())
+	{
+		if (a[i] < b[j])
+		{
+			r.push_back(a[i]);
+			++i;
+		}
+		else if (a[i] == b[j])
+		{
+			r.push_back(a[i]);
+			++i;
+			++j;
+		}
+		else if (a[i] > b[j])
+		{
+			r.push_back(b[j]);
+			++j;
+		}
+	}
+	if (i == a.length())
+		for (j; j < b.length(); ++j) r.push_back(b[j]);
+	else
+		for (i; i < a.length(); ++i) r.push_back(a[i]);
+	return r;
+}
+string arrayR(string a, string b, string c)
+{
+	string r;
+	r = razn(c, b);
+	r = AorB(r, a);
+	return r;
+}
+
+vector<bool> raznBin(vector<bool> a, vector<bool> b)
+{
+	vector<bool> r;
+	r.resize(a.size());
+	int i = 0;
+	while (i < a.size())
+	{
+		if (a[i] == b[i]) r[i] = false;
+		else r[i] = a[i];
+		++i;
+	}
+	return r;
+}
+vector<bool> AorBBin(vector<bool> a, vector<bool> b)
+{
+	vector<bool> r;
+	r.resize(a.size());
+	int i = 0;
+	while (i < a.size())
+	{
+		if (a[i] == true || b[i] == true) r[i] = true;
+		else r[i] = false;
+		++i;
+	}
+	return r;
+}
+vector<bool> arrayRBin(vector<bool> a, vector<bool> b, vector<bool> c)
+{
+	vector<bool> r;
+	r = raznBin(c, b);
+	r = AorBBin(r, a);
+	return r;
+}
+
+string sort(string a)
+{
+	for (int i = 0; i < a.length(); ++i)
+	{
+		bool flag = false;
+		for (int j = 0; j < a.length() - 1; ++j)
+		{
+			if (a[j + 1] < a[j])
+			{
+				a[j + 1] += a[j];
+				a[j] = a[j + 1] - a[j];
+				a[j + 1] -= a[j];
+				flag = true;
+			}
+		}
+		if (!flag) break;
+	}
+	return a;
+}
+string clean(string a)
+{
+	string clean_a;
+	int i;
+	for (i = 0; i < a.length() - 1; ++i)
+	{
+		if (a[i] != a[i + 1]) clean_a.push_back(a[i]);
+	}
+	clean_a.push_back(a[i]);
+	return clean_a;
+}
+vector<bool> makeVector(string a1, string a2)
+{
+	vector<bool> v;
+	v.resize(a1.size());
+	int i = 0, j = 0, count = 0;
+	while (i < a1.length())
+	{
+		if (j >= a2.length() || a1[i] < a2[j])
+		{
+			v[count] = false;
+			++i;
+		}
+		else if (a1[i] == a2[j])
+		{
+			v[count] = true;
+			++i;
+			++j;
+		}
+		else
+		{
+			v[count] = false;
+			++j;
+		}
+		++count;
+	}
+	return v;
+}
+
+//////////////////////////////////////////////////////////////////////
+
+Alphabet::Alphabet()
+{
+	set();
+	a = sort(str);
+	a = clean(a);
 }
